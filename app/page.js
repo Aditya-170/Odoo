3ba10eadcee921597778f435672b39b691285fc1
Aditya-Tiwari-6186 <142735376+Aditya-Tiwari-6186@users.noreply.github.com>
2025-07-12 12:12:34 +0530

    Update page.js to redirect to home

--- app/page.js
+++ app/page.js
@@ -1,17 +1,5 @@
-import Navbar from "@/components/Navbar";
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)] px-6 py-12 bg-[#140024] text-white text-center">
-        <h1 className="text-4xl md:text-5xl font-extrabold leading-snug">
-          Welcome to <span className="text-[#ec4899]">ReWear</span>!
-        </h1>
-        <p className="mt-4 max-w-xl text-lg md:text-xl text-gray-300">
-          Swap your unused clothes and contribute to a more sustainable world 🌍
-        </p>
-      </main>
-    </>
-  );
+export default function Page() {
+  redirect('/home');
 }
